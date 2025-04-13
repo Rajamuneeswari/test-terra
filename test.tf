ac8c49bd03e1f27464faa7d64ec1d1cf27866232
Rajamuneeswari <raji464m@gmail.com>
2025-04-13 15:56:24 +0530

    1st commit

---
+++ test.tf
@@ -0,0 +1,26 @@
+#Terraform Block
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "3.50.0"
+      }
+    }
+}
+
+#Provider Block
+provider "aws" {
+  region = var.aws_regions_mumbai
+  profile = "default"
+}
+
+#Resource Block
+resource "aws_instance" "test-terra" {
+  ami           = var.ec2_ami_id # Amazon Linux
+  instance_type = var.ec2_instance_type
+  count = var.ec2_instance_count
+  tags = {
+    "Name" = "test-terra"
+  }
+
+}
